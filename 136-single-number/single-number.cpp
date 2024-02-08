a1b853d540a5a79bf609d8964bd29f9b1684bbee
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-02-08 13:31:24 +0530

    Update single-number.cpp

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        
+        //basically xor1 ^ xor1 =0
+        //xor of the same number gives zero
         int n=nums.size();
         int xor1=0;
         for(int i=0;i<n;i++){
