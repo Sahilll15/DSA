434936d49095f0701382d125ef7a180b91306db6
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-02-08 13:30:17 +0530

    Time: 11 ms (87.64%) | Memory: 19.1 MB (44.21%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        map<int, int> hash;
-        int ans;
-
-        for (int i = 0; i < n; i++) {
-            hash[nums[i]]++;
+        
+        int n=nums.size();
+        int xor1=0;
+        for(int i=0;i<n;i++){
+            xor1=xor1 ^ nums[i];
         }
 
-        for(auto it: hash){
-            if(it.second == 1){
-                ans=it.first;
-            }
-        }
-    return ans;
+        return xor1;
     }   
 };
