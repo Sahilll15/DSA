6788a1db2b526b60ab2ad9252e36724e23dd5f81
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-04-07 18:02:06 +0530

    Time: 29 ms (5.55%) | Memory: 33 MB (64.06%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -5,14 +5,10 @@ public:
         vector<int> ans;
 
         for(int i=0;i<words.size();i++){
-            for(int k=0;k<words[i].size();k++){
-               if(words[i][k] == x){
+            if(words[i].find(x) != string:: npos){
                 ans.push_back(i);
-                break;
-               }
-            }   
+            }
         }
-
         return ans;
     }
 };
