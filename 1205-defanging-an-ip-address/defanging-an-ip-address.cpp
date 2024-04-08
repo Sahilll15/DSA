22e38c526ce4058a65d2cd9abcb3f182612b000c
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-04-09 01:33:41 +0530

    Time: 3 ms (25.33%) | Memory: 7 MB (86.81%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res;
+       
+       for(auto c :address){
+        if(c =='.'){
+            res+="[.]";
+        }else{
+            res+=c;
+        }
+       }
+
+       return res;
+    }
+};
