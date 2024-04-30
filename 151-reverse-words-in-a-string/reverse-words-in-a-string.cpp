200941428c91f0a6607dccdf445d4c11ef589860
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-04-30 23:48:29 +0530

    Time: 3 ms (82.50%) | Memory: 9 MB (71.96%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,29 @@
+#include <algorithm> // Include the necessary header for std::reverse
+#include <string>
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string reversedString;
+        string temp;
+        bool wordStarted = false;
+        for(int i = s.size() - 1; i >= 0; i--) {
+            if(s[i] != ' ') {
+                temp += s[i];
+                wordStarted = true;
+            } else {
+                if(wordStarted) {
+                    std::reverse(temp.begin(), temp.end());
+                    reversedString += (reversedString.empty() ? "" : " ") + temp;
+                    temp = "";
+                    wordStarted = false;
+                }
+            }
+        }
+        if(wordStarted) {
+            std::reverse(temp.begin(), temp.end());
+            reversedString += (reversedString.empty() ? "" : " ") + temp;
+        }
+        return reversedString;
+    }
+};
