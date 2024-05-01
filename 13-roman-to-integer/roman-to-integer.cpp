8b200b922a46d149fa181fa28a53e6129b3678f2
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-05-01 12:59:50 +0530

    Time: 16 ms (24.12%) | Memory: 12.9 MB (11.23%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+    map<char,int> mp;
+    int ans=0;
+    mp['I']=1;
+    mp['V']=5;
+    mp['X']=10;
+    mp['L']=50;
+    mp['C']=100;
+    mp['D']=500;
+    mp['M']=1000;
+
+    for(int i=0;i<s.length();i++){
+       if(mp[s[i]]<mp[s[i+1]]){
+         ans-=mp[s[i]];
+       }else{
+        ans+=mp[s[i]];
+       }
+    }
+
+    return ans;
+    }
+};
