a613f36d6a31515c0193c64c7254fb35a5f05c93
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-06-04 19:41:02 +0530

    Time: 15 ms (77.52%) | Memory: 9.6 MB (88.58%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    int numberOfSubstrings(std::string s) {
+        std::vector<int> lastSeen(3, -1); // To store the last seen index of 'a', 'b', and 'c'
+        int cnt = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            lastSeen[s[i] - 'a'] = i; 
+
+            int minLastSeen = std::min({lastSeen[0], lastSeen[1], lastSeen[2]});
+
+            if (minLastSeen != -1) {
+                cnt += minLastSeen + 1; 
+            }
+        }
+
+        return cnt;
+    }
+};
