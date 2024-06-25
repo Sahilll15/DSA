5d7cd54c8da8690f718bdde74714cefa7e5b65b1
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-06-26 00:55:25 +0530

    Time: 27 ms (63.08%) | Memory: 23.5 MB (89.55%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
-    bool isEatPresent(const string& str1, const vector<vector<string>>& arrays) {
-        for (const auto& array : arrays) {
-            for (const auto& word : array) {
-                if (word == str1) {
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
+  
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anagramGroups;
         
