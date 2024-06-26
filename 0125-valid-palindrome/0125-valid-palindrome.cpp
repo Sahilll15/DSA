dd186860f908c3aa9b39ca34540637b846670eeb
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-06-26 17:51:44 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        vector<char> chars;
+        for (char c : s) {
+            if (isalnum(c)) chars.push_back(tolower(c));
+        }
+
+        int left=0;
+        int right=chars.size()-1;
+
+
+        while(left<right){
+            if(chars[left]!=chars[right]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
