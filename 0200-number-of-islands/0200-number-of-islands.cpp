31b1a139a7f47054f532812476d85257447d2500
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-06-30 18:47:34 +0530

    Time: 31 ms (43.97%), Space: 15.8 MB (80.85%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,44 @@
 class Solution {
 public:
+    void dfs(int i, int j, vector<vector<char>>& grid) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
+            return;
+        }
+
+        // Mark the cell as visited by setting it to '0'
+        grid[i][j] = '0';
+
+        // Possible directions to move in the grid
+        int delRow[] = {0, +1, 0, -1};
+        int delCol[] = {-1, 0, +1, 0};
+
+        // Move in all possible directions
+        for (int k = 0; k < 4; k++) {
+            int nrow = i + delRow[k];
+            int ncol = j + delCol[k];
+
+            dfs(nrow, ncol, grid);
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         if (grid.empty() || grid[0].empty()) {
             return 0;
         }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+
+        int numOfIslands = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+                    dfs(i, j, grid);
+                    numOfIslands++; 
                 }
             }
         }
-        
-        return numIslands;
-    }
-    
-private:
-    void dfs(vector<vector<char>>& grid, int i, int j) {
-        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != '1') {
-            return;
-        }
-        
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j); 
-        dfs(grid, i - 1, j); 
-        dfs(grid, i, j + 1); 
-        dfs(grid, i, j - 1); 
+
+        return numOfIslands;
     }
 };
