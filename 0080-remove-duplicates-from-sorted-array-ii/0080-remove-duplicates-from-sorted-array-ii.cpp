282c36ff616cc3c4fe4b62312825b548f73fe5eb
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-05 22:18:34 +0530

    Time: 3 ms (72.98%), Space: 15.1 MB (5.75%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.empty()) return 0;
+
+        unordered_map<int,int>mpp;
+
+
+        int j=0;
+
+        for(int i=0;i<nums.size();i++){
+                mpp[nums[i]]++;
+
+                if(mpp[nums[i]]<=2){
+                    nums[j++]=nums[i];
+                }
+        }
+
+        return j;
+    }
+};
