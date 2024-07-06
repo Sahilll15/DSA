051df6122557c631c6402060c1239fb1746349b5
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-06 13:07:34 +0530

    Time: 25 ms (67.68%), Space: 30.5 MB (57.41%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -4,14 +4,17 @@ public:
         int subArrayLen = INT_MAX;
         int left = 0;
         int currentSum = 0;
+        int right = 0;
         
-        for (int right = 0; right < nums.size(); ++right) {
+        while (right < nums.size()) {
             currentSum += nums[right];
             
             while (currentSum >= target) {
                 subArrayLen = min(subArrayLen, right - left + 1);
                 currentSum -= nums[left++];
             }
+            
+            right++;
         }
         
         return (subArrayLen == INT_MAX) ? 0 : subArrayLen;
