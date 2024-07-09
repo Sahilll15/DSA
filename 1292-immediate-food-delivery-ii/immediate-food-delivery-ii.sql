2a7dff1bdb5cf3de49b2a3b5bed756a499d5ddab
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-09 20:22:59 +0530

    Time: 575 ms (74.90%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,10 @@
+select
+  round(avg(order_date=customer_pref_delivery_date)* 100,2) as immediate_percentage
+  from Delivery
+  where 
+  (customer_id,order_date) in (
+    select customer_id,min(order_date)
+    from Delivery
+    group by customer_id
+  )
+  ;
