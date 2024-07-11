f69c0285cfecc0175eb1f511e42d2ccd3a4b76d9
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-12 00:43:25 +0530

    Time: 254 ms (5.05%) | Memory: 13 MB (54.10%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
-        unordered_map<int, int> mpp;
-        vector<int> ans;
 
-        for (int i = 0; i < nums.size(); ++i) {
-            mpp[nums[i]] = i;
-        }
-
-        for (int i = 0; i < nums.size(); ++i) {
-            int value = target - nums[i];
-            
-            if (mpp.find(value) != mpp.end() && mpp[value] != i) {
-                ans.push_back(i);
-                ans.push_back(mpp[value]);
-                break; 
+            vector<int>ans;
+            for(int i=0;i<nums.size();i++){
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[i]+nums[j]==target){
+                       ans.push_back(i);
+                       ans.push_back(j);
+                    }
+                }
             }
-        }
+            return ans;
 
-        return ans;
     }
 };
