f94e8ca4a0fec36a75d2569b085a1525c5d0fb72
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-12 02:07:25 +0530

    Time: 120 ms (58.74%) | Memory: 74.7 MB (20.22%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -2,43 +2,34 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
             int n=nums.size();
-            if(n==0) return n;
-            //set the longest to 1 as the longest element will atleast be 1 
+
+            if(n==0){
+                return 0;
+            }
             int longest=1;
-            //declare a set
-            unordered_set<int> st;
 
+            unordered_set<int>st;
 
-            //insert the elements into the set
-            for(int i=0;i<n;i++){
+            for(int i=0;i<nums.size();i++){
                 st.insert(nums[i]);
             }
 
-        //itearete over the array
+
             for(auto it:st){
-                //check if the previous elemnt of the current element it not 
-                //present into the array
 
-                if(st.find(it -1) == st.end()){
-                    //it means its the first elemnt in the range
-
+                if(st.find(it-1)==st.end()){
                     int cnt=1;
                     int x=it;
 
-                //check if the next elemnt of the number is preset
-                //consider 101 for 100
-                //and loop until the element it not present
-                    while(st.find(x+1) != st.end()){
-                        //increse the count size
+                    while(st.find(x+1)!=st.end()){
                         cnt++;
-
-                        //move to the next element in the set
                         x=x+1;
                     }
-                    //check if this sequence is the longest
-                    longest=max(longest,cnt);
+                longest=max(longest,cnt);
+                    
                 }
             }
-    return longest;
+
+            return longest;
     }
 };
