fbccd1b01fde965836b9ddd3316966320bce6668
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-12 02:11:58 +0530

    Time: 9 ms (21.30%) | Memory: 9.3 MB (11.68%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         vector<char> chars;
-        for (char c : s) {
+        
+   for (char c : s) {
             if (isalnum(c)) chars.push_back(tolower(c));
         }
 
         int left=0;
         int right=chars.size()-1;
 
-
-        while(left<right){
+        while(left<=right){
             if(chars[left]!=chars[right]){
                 return false;
             }
+
             left++;
             right--;
         }
