bf2fe11b1ac1b7a43d88f9730b6cad6f6517f886
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-07-12 21:34:23 +0530

    Time: 3 ms (66.28%), Space: 12.4 MB (10.52%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool bs(vector<int> arr, int target) {
+        bool bs(vector<int> arr, int target) {
         int left = 0;
         int right = arr.size() - 1;
 
@@ -16,22 +16,21 @@ public:
             }
         }
         return false;
-    }
-
+        }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+            int n=matrix.size();
+            int m=matrix[0].size();
 
-        for (int i = 0; i < n; i++) {
-            if (target > matrix[i][m - 1]) {
-                continue;  
-            } else if (target <= matrix[i][m - 1]) {
-                if (bs(matrix[i], target) == true) {
-                    return true;
+            for(int i=0;i<n;i++){
+                if(target>matrix[i][m-1]){
+                    continue;
+                }else if (target <= matrix[i][m-1]){
+                    if(bs(matrix[i],target)==true){
+                        return true;
+                    }
                 }
             }
-        }
 
-        return false;
+            return false;
     }
 };
