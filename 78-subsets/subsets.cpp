e577780b91bd076ad182741867edf9535539f42c
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-08-01 00:14:10 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (41.75%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    void helper(vector<int> nums,vector<vector<int>> &ans,vector<int> temp,int n,int p){
-        if(n==p){
-            ans.push_back(temp);
-            return;
+    void backTrack(vector<vector<int>> &ans,vector<int>temp,vector<int> &nums,int start){
+
+        ans.push_back(temp);
+
+        for(int i=start;i<nums.size();i++){
+            temp.push_back(nums[i]);
+            backTrack(ans,temp,nums,i+1);
+            temp.pop_back();
         }
-
-        helper(nums,ans,temp,n,p+1);
-        temp.push_back(nums[p]);
-        helper(nums,ans,temp,n,p+1);
-
 
     }
     vector<vector<int>> subsets(vector<int>& nums) {
+    
         vector<vector<int>> ans;
-        vector<int> temp;
+        vector<int>temp;
 
-        int p=0;
+        backTrack(ans,temp,nums,0);
 
-        int n=nums.size();
-
-        helper(nums,ans,temp,n,p);
         return ans;
     }
 };
