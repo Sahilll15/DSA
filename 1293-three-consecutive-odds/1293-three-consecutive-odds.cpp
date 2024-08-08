d8aabeace5466da66a777dcc8e710993a0aa9bb4
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2024-08-08 16:06:48 +0530

    Time: 6 ms (26.13%), Space: 10.9 MB (13.15%) - LeetHub

---
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool threeConsecutiveOdds(vector<int> &arr)
+    {
+        for (int i = 2; i < arr.size(); i++)
+        {
+            if (arr[i] % 2 && arr[i - 1] % 2 && arr[i - 2] % 2)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
