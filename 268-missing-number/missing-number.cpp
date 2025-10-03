7d2ba6144126ee3db0b76c77376290bbaca51317
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-03 21:27:57 +0530

    Time: 17 ms (9.18%) | Memory: 21.7 MB (70.14%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(),nums.end());
         int start=0;
         int end=nums.size()-1;
-        int pos=-1;
+
         while (start <= end) {
             int mid = start + (end - start) / 2;
 
