2a36662c0564919dbe5924335feb437b809f878c
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-03 21:27:49 +0530

    Time: 3 ms (20.99%) | Memory: 21.9 MB (13.89%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != i) {
-                return i;
+        sort(nums.begin(),nums.end());
+        int start=0;
+        int end=nums.size()-1;
+        int pos=-1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > mid) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
         }
 
-        return nums.size(); 
+        return start;
     }
 };
