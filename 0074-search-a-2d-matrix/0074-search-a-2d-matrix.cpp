46b28e5a00ef73e2ed8e40f8e317707392765525
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-10 21:01:50 +0530

    Time: 0 ms (100%), Space: 13.2 MB (97.78%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,32 +4,30 @@ public:
         int ans = 0;
         int m = matrix.size();
         int n = matrix[0].size();
-        for (int i = 0; i < m; i++) {
 
-            if (target >= matrix[i][0] && target <= matrix[i][n - 1]) {
-                ans = binary_search(matrix[i], target);
-            }
-        }
+        int start=0;
+        int end= m * n -1;
 
-        return ans;
-    }
+        while(start<=end){
+            int mid=start+(end-start)/2;
 
-    bool binary_search(vector<int>& matrix, int target) {
-        int start = 0;
-        int end = matrix.size() - 1;
+            int row=mid/n;
+            int col=mid % n;
 
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
+            int midValue=matrix[row][col];
 
-            if (matrix[mid] == target) {
+            if(midValue==target){
                 return true;
-            } else if (matrix[mid] > target) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+            }else if(midValue < target){
+                start=mid+1;
+            }else{
+                end=mid-1;
             }
         }
+      
 
-        return false;
+      return false;
     }
+
+   
 };
