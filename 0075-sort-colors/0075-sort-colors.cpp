819280717202a09d37d665eb60195a8689eee9fe
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-21 14:29:38 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.95%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //using dutch flag algoritm
-
-        int high=nums.size()-1;
+        int n=nums.size();
+        int high=n-1;
         int mid=0;
         int low=0;
 
         while(mid<=high){
             if(nums[mid]==0){
                 swap(nums[mid],nums[low]);
-                mid++;
                 low++;
+                mid++;
             }else if(nums[mid]==1){
                 mid++;
             }else{
