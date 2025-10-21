3964786881392980312db8baa99497e6e0a2768e
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-21 14:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.03%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n=nums.size();
+        //using dutch flag algoritm
 
-        int cnt0=0,cnt1=0,cnt2=0;
-        vector <int> ans;
-        for(int i=0;i<n;i++){
-            if(nums[i]== 0){
-                cnt0++;
-            }else if(nums[i] == 1){
-                cnt1++;
+        int high=nums.size()-1;
+        int mid=0;
+        int low=0;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
             }else{
-                cnt2++;
+                swap(nums[mid],nums[high]);
+                high--;
             }
         }
-
-        for (int i = 0; i < cnt0; i++) nums[i] = 0; 
-
-    for (int i = cnt0; i < cnt0 + cnt1; i++) nums[i] = 1; 
-
-    for (int i = cnt0 + cnt1; i < n; i++) nums[i] = 2;
-
-      
-
-        
     }
 };
