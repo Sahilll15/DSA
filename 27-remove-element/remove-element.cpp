f42595131616a18b65c26872c4c869cc9061b3da
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-23 13:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (10.04%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int index = 0;
-        for(int i = 0; i< nums.size(); i++){
-            if(nums[i] != val){
-                nums[index] = nums[i];
-                index++;
+        int k=0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
             }
         }
-        return index;
+
+        return k;
     }
 };
