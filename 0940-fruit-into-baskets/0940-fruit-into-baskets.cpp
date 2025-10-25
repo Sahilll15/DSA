6ff96a25e141e4d3acf16ecdf1bddaa4fb936647
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-25 19:06:37 +0530

    Time: 49 ms (24.22%), Space: 75.2 MB (39.34%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
+        unordered_map<int,int> basket;
+        
+        int i=0;
         int maxFruits=0;
 
-
-        list<int> l1;
-        list<int> l2;
-        int i=0;
         for(int j=0;j<fruits.size();j++){
-            if(l1.empty() || l1.front()==fruits[j]){
-                l1.push_back(fruits[j]);
-            }else if (l2.empty() || l2.front()==fruits[j]){
-                l2.push_back(fruits[j]);
-            }else{
-             while (!l1.empty() && !l2.empty()) {
-                    if (fruits[i] == l1.front())
-                        l1.pop_front();
-                    else
-                        l2.pop_front();
-                    i++;
-                }
+            basket[fruits[j]]++;
 
-               if (l1.empty())
-                    l1.push_back(fruits[j]);
-                else
-                    l2.push_back(fruits[j]);
+            while(basket.size()>2){
+                basket[fruits[i]]--;
+                if(basket[fruits[i]]==0){
+                    basket.erase(fruits[i]);
+                }
+                i++;
             }
-            
-           maxFruits = max(maxFruits, (int)(l1.size() + l2.size()));
+
+            maxFruits=max(maxFruits,j-i+1);
         }
 
+
         return maxFruits;
-    }
+    }   
 };
