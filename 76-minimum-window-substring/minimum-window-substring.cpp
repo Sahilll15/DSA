2eee7b7253ea4df34cecfc2716efb49b2fb0cc2f
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-26 01:10:24 +0530

    Time: 12 ms (44.77%) | Memory: 11.6 MB (42.96%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,40 +1,43 @@
-
 class Solution {
 public:
     string minWindow(string s, string t) {
-        if (s.empty() || t.empty()) return "";
-
-        unordered_map<char, int> tFreq, windowFreq;
-        for (char c : t) tFreq[c]++;
-
-        int l = 0, r = 0, required = tFreq.size(), formed = 0;
-        int minLen = INT_MAX, minStart = 0;
-        
-        while (r < s.size()) {
-            char c = s[r];
-            windowFreq[c]++;
-            
-            if (tFreq.find(c) != tFreq.end() && windowFreq[c] == tFreq[c]) {
-                formed++;
-            }
-
-            while (l <= r && formed == required) {
-                char ch = s[l];
-                
-                if (r - l + 1 < minLen) {
-                    minLen = r - l + 1;
-                    minStart = l;
-                }
-
-                windowFreq[ch]--;
-                if (tFreq.find(ch) != tFreq.end() && windowFreq[ch] < tFreq[ch]) {
-                    formed--;
-                }
-                l++;
-            }
-            r++;
+        unordered_map<char,int> tMap;
+         unordered_map<char,int> sMap;
+        for(char ch:t){
+            tMap[ch]++;
         }
 
-        return minLen == INT_MAX ? "" : s.substr(minStart, minLen);
+        int i = 0;
+        int minLen = INT_MAX;
+        int start = 0; 
+        int formed = 0;
+        for(int j=0;j<s.size();j++){
+            char ch=s[j];
+            if(tMap.count(ch)){
+                sMap[ch]++;
+                if(sMap[ch]==tMap[ch]){
+                    formed++;
+                }
+            }
+
+            while(formed==tMap.size()){
+                if(j-i+1 <minLen){
+                    minLen=j-i+1;
+                    start=i;
+                }
+
+                char leftChar=s[i];
+
+                if(sMap.count(leftChar)){
+                    sMap[leftChar]--;
+                    if(sMap[leftChar]<tMap[leftChar]){
+                        formed--;
+                    }
+                }
+                i++;
+            }
+        }
+
+        return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
     }
 };
