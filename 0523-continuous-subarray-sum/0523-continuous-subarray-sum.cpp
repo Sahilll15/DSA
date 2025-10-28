6d7ff53140b228695fb9a239bb757530a071f9b0
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-29 00:57:27 +0530

    Time: 75 ms (72.34%), Space: 146.2 MB (81.48%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -3,11 +3,11 @@ public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int,int> mp;
         mp[0]=-1;
-
         int sum=0;
 
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
+
             if(mp.count(sum%k)){
                 if(i-mp[sum%k]>=2){
                     return true;
@@ -16,7 +16,6 @@ public:
                 mp[sum%k]=i;
             }
         }
-
         return false;
     }
 };
