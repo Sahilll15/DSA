0c16316cf80ef0c26cd114c84a75a78bdfbb4de7
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-10-30 10:09:59 +0530

    Time: 0 ms (100%), Space: 17.8 MB (24.04%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maxProductVal = nums[0];
-        int currentMax = nums[0];
-        int currentMin = nums[0];
+        int n=nums.size();
+        int leftProduct=1;
+        int rightProduct=1;
+        int ans=nums[0];
 
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < 0) {
-                swap(currentMax, currentMin);
-            }
+        for(int i=0;i<n;i++){
+            leftProduct=leftProduct ==0 ? 1: leftProduct;
+            rightProduct=rightProduct==0 ? 1:rightProduct;
 
-            int tempMax = max(nums[i], currentMax * nums[i]);
-            int tempMin = min(nums[i], currentMin * nums[i]);
+            leftProduct=leftProduct*nums[i];
+            rightProduct=rightProduct*nums[n-1-i];
 
-            currentMax = tempMax;
-            currentMin = tempMin;
-
-            maxProductVal = max(maxProductVal, currentMax);
+            ans=max(ans,max(leftProduct,rightProduct));
         }
 
-        return maxProductVal;
+        return ans;
     }
 };
