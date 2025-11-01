2a21703304607e85d70b0c62ff11dae2fc719a3e
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-01 19:47:54 +0530

    Time: 91 ms (82.16%), Space: 263.4 MB (58.72%) - LeetHub

--- 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/3501-delete-nodes-from-linked-list-present-in-array.cpp
@@ -1,29 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        unordered_map<int, int> values;
-        for (auto it : nums) {
-            values[it]++;
-        }
+        unordered_set<int> st(nums.begin(), nums.end()); 
 
-        ListNode* dummy = new ListNode(-1);
-        dummy->next = head;
-        ListNode* prev = dummy;
-        ListNode* temp = head;
+        ListNode dummy(0);
+        dummy.next = head;
 
-        while (temp != nullptr) {
-            if (values.find(temp->val) != values.end()) {
-                prev->next = temp->next;
-                delete temp;
-                temp = prev->next;
-            } else {
-                prev = temp;
-                temp = temp->next;
+         ListNode* prev = &dummy;
+        ListNode* current = head;
+
+        while(current!=nullptr){
+            if(st.count(current->val)){
+                prev->next=current->next;
+            }else{
+                prev=current;
             }
+            current=current->next;
         }
 
-        ListNode* newHead = dummy->next;
-        delete dummy;
-        return newHead;
+        return dummy.next;
     }
 };
