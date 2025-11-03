4170b173ccbbb5969b27fa0f01d8b9049d7baead
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-03 22:58:38 +0530

    Time: 2 ms (39.56%), Space: 77.3 MB (13.72%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,43 +1,30 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy;              
+        ListNode* ans = &dummy;       
+
         ListNode* head1 = l1;
         ListNode* head2 = l2;
+
         int carry = 0;
 
-        ListNode dummy;
-        ListNode* ans = &dummy;
+        while (head1 != nullptr || head2 != nullptr) {
+            int firstValue = (head1 != nullptr) ? head1->val : 0;
+            int secondValue = (head2 != nullptr) ? head2->val : 0;
 
-        while (head1 != nullptr || head2 != nullptr || carry != 0) {
-            int firstNum = 0;
-            int secondNum = 0;
+            int finalValue = firstValue + secondValue + carry;
+            carry = finalValue / 10;
 
-            if (head1 != nullptr) {
-                firstNum = head1->val;
-                head1 = head1->next;
-            }
-            if (head2 != nullptr) {
-                secondNum = head2->val;
-                head2 = head2->next;
-            }
+            ans->next = new ListNode(finalValue % 10);
+            ans = ans->next; 
 
-            int val = firstNum + secondNum + carry;
-            carry = val / 10;
-            val = val % 10;
-
-            ans->next = new ListNode(val);
-            ans = ans->next;
+            if (head1) head1 = head1->next;
+            if (head2) head2 = head2->next;
         }
+
+        if (carry > 0)
+            ans->next = new ListNode(carry);
 
         return dummy.next;
     }
