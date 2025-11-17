f9cacca7129ae7b9247003e8d072167b47fb2c43
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-17 08:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.89%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,24 +1,28 @@
-#include <algorithm> // for std::max
-#include <cmath>     // for std::abs
 
 class Solution {
 public:
-    int getHeight(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
+    bool isBalanced(TreeNode* root) {
+        if(root==nullptr) return true;
+        int leftHeight=getHeight(root->left);
+        int rightHeight=getHeight(root->right);
+        if(leftHeight== -1 || rightHeight == -1){
+            return false;
         }
+        return abs(leftHeight-rightHeight)<=1;
+    }   
+
+    int getHeight(TreeNode* root){
+        if(root==nullptr) return 0;
+        int left=getHeight(root->left);
+        int right=getHeight(root->right);
         
-        int lh = getHeight(root->left);
-        int rh = getHeight(root->right);
-        
-        if (lh == -1 || rh == -1 || std::abs(lh - rh) > 1) {
+         if(left== -1 || right == -1){
+            return -1;
+        }
+        if(abs(left-right)>1){
             return -1;
         }
 
-        return 1 + std::max(lh, rh);
-    }
-
-    bool isBalanced(TreeNode* root) {
-        return getHeight(root) != -1;
+        return max(left,right)+1;
     }
 };
