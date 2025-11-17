09413e59b26e6e499809b4ee89c6a65e7223ec7d
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-17 08:57:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (73.79%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.cpp
@@ -1,30 +1,23 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr, right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    int diameter = 0;
-
-    int maxDepth(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
-        }
-        int lh = maxDepth(root->left);
-        int rh = maxDepth(root->right);
-        diameter = std::max(diameter, lh + rh);
-        return 1 + std::max(lh, rh);
-    }
-
     int diameterOfBinaryTree(TreeNode* root) {
-        maxDepth(root); 
-        return diameter;
+        int maxVal=0;
+
+        if(root==nullptr) return 0;
+        getHeight(root,maxVal);
+        return maxVal;
     }
+
+    int getHeight(TreeNode* root,int& maxVal){
+        if(root==nullptr) return 0;
+
+        int leftHeight = getHeight(root->left, maxVal);
+        int rightHeight = getHeight(root->right, maxVal);
+
+        maxVal=max(maxVal,leftHeight+rightHeight);
+
+        return max(leftHeight,rightHeight)+1;
+    }
+
 };
