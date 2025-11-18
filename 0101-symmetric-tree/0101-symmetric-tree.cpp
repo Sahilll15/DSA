1a1d1b5998ee5bcf291439c68d860cb6d77558bb
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-18 08:43:13 +0530

    Time: 0 ms (100%), Space: 18.4 MB (86.2%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,45 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    void leftTraversal(TreeNode* root, vector<int>& arr) {
-        if (root != nullptr) {
-            arr.push_back(root->val);
-            leftTraversal(root->left, arr);
-            leftTraversal(root->right, arr);
-        } else {
-            arr.push_back(INT_MIN); 
-        }
-    }
-
-    void rightTraversal(TreeNode* root, vector<int>& arr) {
-        if (root != nullptr) {
-            arr.push_back(root->val);
-            rightTraversal(root->right, arr);  
-            rightTraversal(root->left, arr);   
-        } else {
-            arr.push_back(INT_MIN); 
-        }
-    }
-
     bool isSymmetric(TreeNode* root) {
-        if (root == nullptr) return true;
+        if(root==nullptr) return false;
 
-        vector<int> leftPart;
-        vector<int> rightPart;
 
-        leftTraversal(root->left, leftPart);
-        rightTraversal(root->right, rightPart);
+        return isSame(root->left,root->right) == 1  ;
+    }
 
-        return leftPart == rightPart;
+    int isSame(TreeNode* root1,TreeNode* root2){
+        if(root1==nullptr && root2==nullptr) return 1;
+        
+        if(root1==nullptr || root2==nullptr) return -1;
+
+        if(root1->val != root2->val){
+            return -1;
+        }
+        int val1=isSame(root1->left,root2->right);
+        int val2=isSame(root1->right,root2->left); 
+
+        if(val1 == -1 || val2==-1){
+            return -1;
+        }
+
+        return 1;
     }
 };
