a2f5ecd1289b2d5e54bb8736cd38097415687282
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-19 09:05:02 +0530

    Time: 0 ms (100%), Space: 19.7 MB (73.06%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,70 +1,38 @@
+
 class Solution {
 public:
-    
-    #define ll long long
-    
     int widthOfBinaryTree(TreeNode* root) {
-        
-        // declare a queue
-       
-        queue<pair<TreeNode*, int>> q;
-        
-        // push the root into queue
-        
-        q.push({root, 0});
-        
-        int max_width = INT_MIN;
-        
-        // now run a bfs 
-        
-        while(!q.empty())
-        {
+        if (!root) return 0;
+
+        long long maxWidth = 0;
+        queue<pair<TreeNode*, long long>> q;
+
+        q.push({root,1});
+
+        while(!q.empty()){
             int size = q.size();
-            
-            int min_idx = q.front().second;
-            
-            int first_idx, last_idx;
-            
-            for(int i = 0; i < size; i++)
-            {
-                TreeNode* curr_node = q.front().first;
-                
-                int curr_idx = q.front().second;
-                
-                curr_idx -= min_idx;
-                
-                q.pop();
-                
-                if(i == 0)
-                {
-                    first_idx = curr_idx;
-                }
-                
-                if(i == size - 1)
-                {
-                    last_idx = curr_idx;
-                }
-                
-                // push the left child
-                
-                if(curr_node -> left)
-                {
-                    q.push({curr_node -> left, (ll) 2 * curr_idx + 1});
-                }
-                
-                // push the right child
-                
-                if(curr_node -> right)
-                {
-                    q.push({curr_node -> right, (ll) 2 * curr_idx + 2});
-                }
+            long long start=q.front().second;
+            long long end=start;
+
+            for(int i=0;i<size;i++){
+                 auto [node, idx] = q.front();
+                 q.pop();
+
+                 idx -= start;
+
+                 end = idx;
+
+                 if(node->left){
+                    q.push({node->left,2*idx});
+                 }
+                  if(node->right){
+                    q.push({node->right,2*idx+1});
+                 }
             }
-            
-            // update max width
-            
-            max_width = max(max_width, last_idx - first_idx + 1);
+
+            maxWidth=max(maxWidth,end+1);
         }
-        
-        return max_width;
+
+        return maxWidth;
     }
 };
