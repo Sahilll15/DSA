2e7310b7a6aeae20525d8b1f1cb47db1c1ed4e92
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-11-20 09:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 15.4 MB (68.71%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    void makeParents(TreeNode* root, unordered_map<TreeNode*, TreeNode*>& parent) {
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node->left) {
+                parent[node->left] = node;
+                q.push(node->left);
+            }
+            if (node->right) {
+                parent[node->right] = node;
+                q.push(node->right);
+            }
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*, TreeNode*> parent;
+        makeParents(root, parent);
+
+        unordered_map<TreeNode*, bool> visited;
+        queue<TreeNode*> q;
+
+        q.push(target);
+        visited[target] = true;
+
+        int currDist = 0;
+
+        while (!q.empty()) {
+            int size = q.size();
+            if (currDist == k) break;
+            currDist++;
+
+            while (size--) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (node->left && !visited[node->left]) {
+                    visited[node->left] = true;
+                    q.push(node->left);
+                }
+                if (node->right && !visited[node->right]) {
+                    visited[node->right] = true;
+                    q.push(node->right);
+                }
+                if (parent[node] && !visited[parent[node]]) {
+                    visited[parent[node]] = true;
+                    q.push(parent[node]);
+                }
+            }
+        }
+
+        vector<int> ans;
+        while (!q.empty()) {
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
