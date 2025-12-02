2644904b59f431faf3158bb92ebf8bb947909c07
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2025-12-02 08:53:09 +0530

    Time: 4 ms (23.35%), Space: 52.4 MB (34.72%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n=nums.size();
-
-        if(n==0){
-            return 0;
-        }
-
-        int maxReach=0;
-
+        int maxIndex=0;
         for(int i=0;i<nums.size();i++){
-            if(i>maxReach) return false;
-            maxReach=max(maxReach,i+nums[i]);
-            if(maxReach>= n-1) return true;
+            if(i>maxIndex) return false;
+            maxIndex=max(maxIndex,i+nums[i]);
         }
-
-        return false;
+        return true;
     }
 };
