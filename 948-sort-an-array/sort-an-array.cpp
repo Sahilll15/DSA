566b837aeda6743c6945aff07654f3bc3e5cc0c6
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-03-03 16:36:08 +0530

    Time: 589 ms (33.56%) | Memory: 295.8 MB (7.73%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-
-    void merge(vector<int>& nums,int low,int mid,int high){
+    void merge(vector<int>&nums,int low,int mid,int high){
+        vector<int> temp;
         int left=low;
         int right=mid+1;
 
-        vector<int> temp;
-
-        while(left<=mid && right<=high){
+        while(left <=mid && right<=high){
             if(nums[left]<=nums[right]){
                 temp.push_back(nums[left]);
                 left++;
             }else{
                 temp.push_back(nums[right]);
                 right++;
-            }   
+            }
         }
 
         while(left<=mid){
@@ -35,6 +33,7 @@ public:
         if(low>=high){
             return;
         }
+
         int mid=low+(high-low)/2;
         mergeSortHelper(nums,low,mid);
         mergeSortHelper(nums,mid+1,high);
@@ -42,9 +41,7 @@ public:
     }
     vector<int> sortArray(vector<int>& nums) {
         int n=nums.size();
-
         mergeSortHelper(nums,0,n-1);
-
         return nums;
     }
 };
