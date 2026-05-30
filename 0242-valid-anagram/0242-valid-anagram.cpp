2ca784eb70f937911fd805bf82466c74d99de8c7
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-05-31 01:04:06 +0530

    Time: 0 ms (100%), Space: 9.6 MB (91.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,13 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-            sort(s.begin(),s.end());
-            sort(t.begin(),t.end());
-
-            if(s==t){
-                return true;
-            }
-
+        int count[26]={0};
+        if(s.size()!=t.size()){
             return false;
+        }
+
+        for(char c:s){
+            count[c-'a']++;
+        }
+
+        for(char c:t){
+            count[c-'a']--;
+        }
+
+        for(int v:count){
+            if(v!=0){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
