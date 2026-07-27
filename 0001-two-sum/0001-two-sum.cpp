0327ab4f89414ded38334ec1d7838266f5df01f7
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-07-27 10:18:51 +0530

    Time: 0 ms (100%), Space: 14.9 MB (21.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> seen;
         
-        vector<int> ans;
-
-        unordered_map<int,int> Mymap;
-
         for(int i=0;i<nums.size();i++){
-            Mymap[nums[i]]=i;
+            int need=target-nums[i];
+            auto it=seen.find(need);
+            if(it!=seen.end()) return {it->second,i};
+            seen[nums[i]]=i;
         }
+        
 
-        for(int i=0;i<nums.size();i++){
-        int required=target-nums[i];
-        auto it = Mymap.find(required);
-        if (it != Mymap.end() && it->second != i){
-                ans.push_back(i);
-                ans.push_back(Mymap[required]);
-                break;
-            }
-        }
-
-        return ans;
+        return {};
     }
 };
