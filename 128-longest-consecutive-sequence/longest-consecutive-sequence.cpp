c40c8dc4551390ad4955ba1772c3552c70b983d0
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-10 15:31:21 +0530

    Time: 87 ms (31.34%) | Memory: 89.1 MB (25.62%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-            int n=nums.size();
+        unordered_set<int> numSet;
+        if(nums.size()==0) return 0;
+        int maxAns=1;
+        int cnt=1;
+        for(int i=0;i<nums.size();i++){
+            numSet.insert(nums[i]);
+        }
 
-            if(n==0){
-                return 0;
-            }
-            int longest=1;
-
-            unordered_set<int>st;
-
-            for(int i=0;i<nums.size();i++){
-                st.insert(nums[i]);
-            }
-
-
-            for(auto it:st){
-
-                if(st.find(it-1)==st.end()){
-                    int cnt=1;
-                    int x=it;
-
-                    while(st.find(x+1)!=st.end()){
-                        cnt++;
-                        x=x+1;
-                    }
-                longest=max(longest,cnt);
-                    
+        for(auto it:numSet){
+            if(numSet.find(it-1)==numSet.end()){
+                int need=it;
+                while(numSet.find(need+1)!=numSet.end()){
+                    cnt++;
+                    need++;
                 }
+                maxAns=max(maxAns,cnt);
+                cnt=1;
             }
+        }
 
-            return longest;
+        return maxAns;
     }
 };
