348809e6251ffb7811321184ee76ff19dd0ca24f
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-19 00:33:48 +0530

    Time: 1 ms (45.26%) | Memory: 10.8 MB (40.06%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,31 +1,21 @@
 class Solution {
 public:
+    vector<vector<int>>ans;
+    void solve(int index,vector<int>& nums,vector<int>&current){
+            ans.push_back(current);
+        
+
+        for(int i=index;i<nums.size();i++){
+           if(i>index && nums[i]==nums[i-1])continue;
+            current.push_back(nums[i]);
+            solve(i+1,nums,current);
+            current.pop_back();
+        }  
+}
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        set<vector<int>> ansSet;
-        vector<int> ds;
-
-        genSub(0,nums,ds,ansSet);
-
-        vector<vector<int>> ans;
-
-        for (auto &v : ansSet) {
-            ans.push_back(v);
-    }
-
-
+        vector<int>current;
+        sort(nums.begin(),nums.end());
+        solve(0,nums,current);
         return ans;
-    }
-
-    void genSub(int i,vector<int>& nums,vector<int>& ds,set<vector<int>>& ans){
-        if(i==nums.size()){
-            ans.insert(ds);
-            return;
-        }
-
-        ds.push_back(nums[i]);
-        genSub(i+1,nums,ds,ans);
-        ds.pop_back();
-        genSub(i+1,nums,ds,ans);
     }
 };
