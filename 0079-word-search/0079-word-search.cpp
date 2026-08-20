a11ded1daa4eb7ff8399da2bf1323cd78438c8bd
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-20 09:22:18 +0530

    Time: 816 ms (23%), Space: 11.4 MB (43.06%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,33 +1,36 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if (check(i, j, 0, board, word)) return true;
-            }
+    bool solve(int i,int j,int index,vector<vector<char>>& board, string word){
+        if(index==word.size()){
+            return true;
         }
-        return false;
-    }
 
-    bool check(int i, int j, int k, vector<vector<char>>& board, string& word) {
-        if (k == word.size()) return true;
 
         if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size())
             return false;
 
-        if (board[i][j] != word[k])
+        if (board[i][j] != word[index])
             return false;
 
-        char temp=board[i][j];
+        
+        char org=board[i][j];
         board[i][j]='#';
-        bool top = check(i - 1, j, k + 1, board, word);
-        bool right = check(i, j + 1, k + 1, board, word);
-        bool bottom = check(i + 1, j, k + 1, board, word);
-        bool left = check(i, j - 1, k + 1, board, word);
+        bool left=solve(i-1,j,index+1,board,word);
+        bool right=solve(i+1,j,index+1,board,word);
+        bool top=solve(i,j-1,index+1,board,word);
+        bool bottom=solve(i,j+1,index+1,board,word);
+        board[i][j]=org;
 
-        bool found= top || right || bottom || left;
-
-        board[i][j]=temp;
-        return found;
+        return left||right||top||bottom;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[i].size();j++){
+                if(board[i][j]==word[0]){
+                    if(solve(i,j,0,board,word)) return true;
+                }
+            }
+        }
+        return false;
     }
 };
