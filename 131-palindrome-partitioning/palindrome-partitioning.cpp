f59039f4766d0d8333a1d97c91f72e9b8d739f06
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-20 08:45:51 +0530

    Time: 76 ms (16.23%) | Memory: 83.9 MB (17.77%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
-    vector<vector<string>> partition(string s) {
-        vector<vector<string>> ans;
-        vector<string> path;
+    bool isPalindrome(string s){
+        int left=0; int right=s.size()-1;
 
-        genSub(0, s, path, ans);
-
-        return ans;
-    }
-
-    void genSub(int i, string& s, vector<string>& path,
-                vector<vector<string>>& ans) {
-        if (i == s.size()){
-            ans.push_back(path);
-            return;
-        }
-
-        for (int j = i; j < s.size(); j++) {
-            string piece = s.substr(i, j - i + 1);
-            if (isPalindrom(piece)) {
-                path.push_back(piece);
-                genSub(j+ 1, s, path, ans);
-                path.pop_back();
-            }
-        }
-    }
-
-    bool isPalindrom(string piece) {
-        int l = 0;
-        int r = piece.size() - 1;
-        while (l < r) {
-            if (piece[l++] != piece[r--])
+        while(left<=right){
+            if(s[left]!=s[right]){
                 return false;
+            }
+            left++;
+            right--;
         }
 
         return true;
+    }
+    vector<vector<string>> ans;
+    void solve(int index,vector<string>&current,string s){
+        if(index==s.size()){
+            ans.push_back(current);
+            return;
+        }
+
+        for(int i=index;i<s.size();i++){
+            string temp=s.substr(index,i-index+1);
+            bool isPal=isPalindrome(temp);
+            
+            if(isPal){
+                current.push_back(temp);
+                solve(i+1,current,s);
+                current.pop_back();
+            }
+          
+        }
+        
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string>current;
+        solve(0,current,s);
+        return ans;
     }
 };
