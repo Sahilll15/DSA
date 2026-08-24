2ff1c8e64c8a8ee63b7652d822d34d2077ff7fd3
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-25 00:08:53 +0530

    Time: 1672 ms (6.22%) | Memory: 23.8 MB (86.24%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,18 +9,15 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA==nullptr || headB==nullptr) return nullptr;
+        ListNode* PA=headA;
+        ListNode* PB=headB;
 
-        if(headA==nullptr || headB==nullptr ) return nullptr;
-        ListNode* P1=headA;
-        ListNode* P2=headB;
-
-        while(P1 !=P2){
-            P1=(P1) ? P1->next: headA;
-            P2=(P2) ? P2->next: headB;
+        while(PA !=PB){
+            PA=PA? PA->next:headA;
+            PB=PB? PB->next: headB;
         }
 
-        return P1;
-
-
+        return PA;
     }
 };
