2e0a0530e955aeb91c351f8dbec3552fedf69819
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-28 01:27:03 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.26%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int ans=start;
+
+            for(int i=1;i<n;i++){
+                ans=ans^(start+2*i);
+            }
+
+            return ans;
+    }
+};
