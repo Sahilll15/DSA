45b09c85717a5b0d51c8cf73d21ac509957ce037
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-27 23:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (57.28%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-       int n=nums.size();
-            
-            long XOR=0;
-
-            for(int i=0;i<n;i++){
-                XOR=XOR^nums[i];
-            }
-
-            int rightmost=(XOR & (XOR-1)) ^ XOR;
-
-            int XOR1=0,XOR2=0;
-
-             // Traverse the array
-        for(int i=0; i < n; i++) {
         
-            if(nums[i] & rightmost) {
-                XOR1 = XOR1 ^ nums[i];
-            }
-            else {
-                XOR2 = XOR2 ^ nums[i];
+        long XOR=0;
+
+        for(int i=0;i<nums.size();i++){
+            XOR=XOR^nums[i];
+        }
+
+        int rightmost=(XOR & (XOR-1)) ^ XOR;
+
+        int XOR1=0;
+        int XOR2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] & rightmost){
+                XOR1=XOR1^nums[i];
+            }else{
+                XOR2=XOR2^nums[i];
             }
         }
 
-          if(XOR1 < XOR2) return {XOR1, XOR2};
-        return {XOR2, XOR1};
-        }
+        return {
+            XOR2,XOR1
+        };
+    }
 };
