07421f704740e7dc0359df2e36897ef319737628
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-08-29 00:25:22 +0530

    Time: 12 ms (28.95%) | Memory: 44.8 MB (78.20%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cookiesNums = s.size();
-        if(cookiesNums == 0)  return 0;
-        sort(g.begin(), g.end());
         sort(s.begin(), s.end());
+        sort(g.begin(), g.end());
 
-        int maxNum = 0;
-        int cookieIndex = cookiesNums - 1;
-        int childIndex = g.size() - 1;
-        while(cookieIndex >= 0 && childIndex >=0){
-            if(s[cookieIndex] >= g[childIndex]){
-                maxNum++;
-                cookieIndex--;
-                childIndex--;
-            }
-            else{
-                childIndex--;
-            }
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+
+        while(l < g.size() && r < s.size()) {
+            if (s[r] >= g[l]) {
+    ans++;
+    l++;
+    r++;
+} else {
+    r++;
+}
         }
-        return maxNum;
+
+        return ans;
     }
 };
