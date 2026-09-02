351308a1d21f5c7f6c66db162f2283ffa68a8968
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-03 02:05:01 +0530

    Time: 99 ms (71.09%) | Memory: 98.4 MB (29.51%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        long long maxLen = 0;
-        unordered_map<int, int> map;
-        int i = 0;
-
-        long long sum = 0;
-        for (int j = 0; j < nums.size(); j++) {
-            if (map.count(nums[j])) {
-
-                while (map.count(nums[j])) {
-                    sum -= nums[i];
-                    map.erase(nums[i]);
-                    i++;
-                }
+        long long maximum = 0;
+        long long sum=0;
+        int left = 0;
+        unordered_map<int, int> numsMap;
+        for (int right = 0; right < nums.size(); right++) {
+        sum += nums[right];
+        numsMap[nums[right]]++;
+            
+       while (numsMap[nums[right]] > 1) {
+                sum -= nums[left];
+                numsMap[nums[left]]--;
+                left++;
             }
 
-            sum = sum +(long long) nums[j];
-            map[nums[j]] = j;
-
-            while (j - i + 1 > k) {
-                sum -= nums[i];
-                map.erase(nums[i]);
-                i++;
-            }
-
-            if(j-i+1 == k){
-                maxLen = max(maxLen,(long long) sum);
-            }
+        if(right-left+1==k){
+            maximum=max(maximum,sum);
+            numsMap[nums[left]]--;
+            sum-=nums[left];
+            left++;
         }
-
-        return maxLen;
     }
+
+        return maximum;
+    }
+    
 };
