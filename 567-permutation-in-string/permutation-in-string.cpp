b25c1743f0b171b7b30a9a3ce122f6428dda2f89
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-04 01:13:15 +0530

    Time: 4 ms (52.81%) | Memory: 9.9 MB (89.50%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int window=s1.size();
+        vector<int> s1Freq(26, 0);
+        vector<int> windowFreq(26, 0);
 
-        sort(s1.begin(),s1.end());
+        for(int i=0;i<s1.size();i++){
+            s1Freq[s1[i]-'a']++;
+        }
         int left=0;
-        string curr;
         for(int right=0;right<s2.size();right++){
-            curr+=s2[right];
+            windowFreq[s2[right]-'a']++;
             if(right-left+1==window){
-                string windowString=s2.substr(left,window);
-                sort(windowString.begin(),windowString.end());
-                if(windowString==s1){
+                if(windowFreq==s1Freq){
                     return true;
                 }
-                curr.erase(left,1);
+                windowFreq[s2[left]-'a']--;
                 left++;
             }
         }
