8875b27aa99f9bd43d615ed998a6ba8d201b2b0e
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-06 00:15:54 +0530

    Time: 330 ms (7.64%), Space: 180 MB (6.61%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,18 +2,26 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> ans;
-        priority_queue<pair<int, int>> pq; 
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            while (!pq.empty() && pq.top().second <= i - k)
-                pq.pop();
-            
-            pq.push({nums[i], i});
-            
-            if (i >= k - 1)
-                ans.push_back(pq.top().first);
+
+        map<int, int> numsMap;
+
+        int left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            numsMap[nums[right]]++;
+
+            if (right - left + 1 == k) {
+                ans.push_back(numsMap.rbegin()->first);
+                numsMap[nums[left]]--;
+
+                if (numsMap[nums[left]] == 0) {
+                    numsMap.erase(nums[left]);
+                }
+
+                left++;
+            }
         }
-        
+
         return ans;
     }
 };
