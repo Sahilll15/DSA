673996197e29fd2780c3b3a7159e7ee4fd3995d5
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-13 00:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (42.39%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,45 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    void leftTraversal(TreeNode* root, vector<int>& arr) {
-        if (root != nullptr) {
-            arr.push_back(root->val);
-            leftTraversal(root->left, arr);
-            leftTraversal(root->right, arr);
-        } else {
-            arr.push_back(INT_MIN); 
-        }
-    }
-
-    void rightTraversal(TreeNode* root, vector<int>& arr) {
-        if (root != nullptr) {
-            arr.push_back(root->val);
-            rightTraversal(root->right, arr);  
-            rightTraversal(root->left, arr);   
-        } else {
-            arr.push_back(INT_MIN); 
-        }
-    }
-
     bool isSymmetric(TreeNode* root) {
-        if (root == nullptr) return true;
+        if(root==nullptr) return true;
+        return checkSymmetry(root->left,root->right);        
+    }
 
-        vector<int> leftPart;
-        vector<int> rightPart;
+    bool checkSymmetry(TreeNode* p,TreeNode* q){
+        if(p==nullptr && q==nullptr) return true;
 
-        leftTraversal(root->left, leftPart);
-        rightTraversal(root->right, rightPart);
+        if(p==nullptr || q==nullptr) return false;
 
-        return leftPart == rightPart;
+        if(p->val != q->val) return false;
+
+        return checkSymmetry(p->left,q->right) && checkSymmetry(p->right,q->left);
     }
 };
