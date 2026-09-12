c215a6a9ecfb2eb8d46d6da97bf4282b6e9894ed
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-12 20:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (27.50%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -6,13 +6,9 @@ public:
         int left=0;
         int right=0;
 
-        if(root->left!=nullptr){
-            left=maxDepth(root->left);
-        }
-
-        if(root->right!=nullptr){
-            right=maxDepth(root->right);
-        }
+        left=maxDepth(root->left);
+        right=maxDepth(root->right);
+        
 
         return 1+max(left,right);
     }
