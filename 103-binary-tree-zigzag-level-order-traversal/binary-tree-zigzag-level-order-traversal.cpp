c3a869bf1d5199f02ca2c926a81afd80d9df762d
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-13 23:39:38 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (16.87%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -8,28 +8,28 @@ public:
         queue<TreeNode*> q;
 
         q.push(root);
-        bool reversed=false;
+        bool reversed=true;
         while(!q.empty()){
             int size=q.size();
 
-            vector<int> level;
+            vector<int> level(size);
             for(int i=0;i<size;i++){
                 TreeNode* node=q.front();
                 q.pop();
-
-                level.push_back(node->val);
+                int index;
+                if(reversed){
+                     index=i;
+                }else{
+                    index=size-1-i;
+                }
+                level[index]=node->val;
                 if(node->left!=nullptr) q.push(node->left);
                 if(node->right!=nullptr) q.push(node->right);
             }
-                if(reversed){
-                    reverse(level.begin(),level.end());
-                    reversed=false;
-                }else{
-                    reversed=true;
-                }
+                
 
             ans.push_back(level);
-                
+            reversed=!reversed;
         }
         return ans;
     }
