1e8f2c9e859e0a0a4d21e2827619846ad245c5e2
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-21 00:33:20 +0530

    Time: 0 ms (100%), Space: 24.8 MB (7.67%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -4,16 +4,14 @@ public:
     void traversal(TreeNode* root,int k,vector<int>& elements){
         if(root==nullptr) return;
 
-        elements.push_back(root->val);
         traversal(root->left,k,elements);
+         elements.push_back(root->val);
         traversal(root->right,k,elements);
     }
     int kthSmallest(TreeNode* root, int k) {
         vector<int>elements;
 
         traversal(root,k,elements);
-
-        sort(elements.begin(),elements.end());
         return elements[k-1];
     }
 };
