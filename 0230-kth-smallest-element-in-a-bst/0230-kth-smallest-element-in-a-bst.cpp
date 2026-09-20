90fec9df6e876ba0c1d7541a49e2f1b0c79a23c5
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-21 00:30:39 +0530

    Time: 1 ms (10.99%), Space: 24.6 MB (17.24%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,35 +1,19 @@
 
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        int ans;
-        if(root==nullptr){
-            return 0;
-        }
-        pq.push(root->val);
+    void traversal(TreeNode* root,int k,vector<int>& elements){
+        if(root==nullptr) return;
 
-        traversal(root->left,pq);
-        if(pq.size()<k){
-            traversal(root->right, pq);
-        }
-
-
-        while(k>0){
-            ans=pq.top();
-            pq.pop();
-            k--;
-        }
-        return ans;
+        elements.push_back(root->val);
+        traversal(root->left,k,elements);
+        traversal(root->right,k,elements);
     }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>elements;
 
-    void traversal(TreeNode* root,priority_queue<int, vector<int>, greater<int>>& pq){
-        if(root==nullptr){
-            return;
-        }
+        traversal(root,k,elements);
 
-        pq.push(root->val);
-        traversal(root->left,pq);
-        traversal(root->right,pq);
+        sort(elements.begin(),elements.end());
+        return elements[k-1];
     }
 };
