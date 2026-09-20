2f4324b94083f860478e95b1663f9b2d6fab9d76
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-21 00:33:23 +0530

    Time: 0 ms (100.00%) | Memory: 24.8 MB (7.67%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,18 +1,17 @@
 
 class Solution {
 public:
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> inorder;
-        traversal(root,inorder);
-        return inorder[k-1];
-    }
+    void traversal(TreeNode* root,int k,vector<int>& elements){
+        if(root==nullptr) return;
 
-    void traversal(TreeNode* root,vector<int>& inorder){
-        if(root==nullptr){
-            return;
-        }
-        traversal(root->left,inorder);
-        inorder.push_back(root->val);
-        traversal(root->right,inorder);
+        traversal(root->left,k,elements);
+         elements.push_back(root->val);
+        traversal(root->right,k,elements);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>elements;
+
+        traversal(root,k,elements);
+        return elements[k-1];
     }
 };
