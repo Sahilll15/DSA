8402dfc5f67582b0e3327fc70a122222630b8960
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-24 00:56:39 +0530

    Time: 4 ms (24.36%), Space: 20.5 MB (29.86%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    void dfs(int node, vector<vector<int>>& adjList, vector<int>& visited) {
-        visited[node] = 1;
-        for (int neighbor : adjList[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, adjList, visited);
+    void dfs(int node, vector<vector<int>>& adj,vector<int>& vis){
+        vis[node]=1;
+
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                dfs(it,adj,vis);
             }
         }
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<vector<int>> adjList(n);
-        vector<int> visited(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (isConnected[i][j] == 1) {
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
+        int n=isConnected.size();
+        vector<vector<int>> adj(n);
+        vector<int> vis(n);
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
 
-        int cnt = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
                 cnt++;
-                dfs(i, adjList, visited);
+                dfs(i,adj,vis);
             }
         }
 
         return cnt;
-    }
+    }       
 };
