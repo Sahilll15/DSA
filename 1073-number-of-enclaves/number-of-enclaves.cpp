f73b13873faf7aa2393fe54ce19fa0b3a1356af7
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-24 17:57:25 +0530

    Time: 16 ms (15.83%) | Memory: 39.8 MB (11.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,60 +1,57 @@
 class Solution {
 public:
-    bool isBoundary(int r, int c, int n, int m) {
-    return r == 0 || r == n - 1 || c == 0 || c == m - 1;
-    }  
-
-    vector<int> deltaRow={-1,0,+1,0};
-    vector<int> deltaCol={0,+1,0,-1};
-
-    void dfs(int row,int col,vector<vector<int>>& visited,vector<vector<int>>& grid){
-      
-        int n=grid.size();
-        int m=grid[0].size();
-        
-        visited[row][col]=1;
-          grid[row][col]=0;
-
-        for(int i=0;i<4;i++){
-            int nrow=row+deltaRow[i];
-            int ncol=col+deltaCol[i];
-                if (nrow >= 0 && nrow < n &&
-    ncol >= 0 && ncol < m &&
-    grid[nrow][ncol] == 1 &&
-    !visited[nrow][ncol]) {
-    dfs(nrow, ncol, visited, grid);
-}
-
-            
+    void dfs(int i,int j,vector<vector<int>>& vis,
+             vector<vector<int>>& grid){
+        int rows=grid.size();
+        int cols=grid[0].size();
+        if(i<0|| i>=rows || j<0 || j>=cols || grid[i][j]==0 || vis[i][j] == 1){
+            return;
         }
-        
-    }
 
+        vis[i][j]=1;
+
+        
+        int sr[]={-1,0,+1,0};
+        int sc[]={0,+1,0,-1};
+
+        for(int k=0;k<4;k++){
+            dfs(i+sr[k],j+sc[k],vis,grid);
+        }
+    }
     int numEnclaves(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>> visited(n,vector<int>(m,0));
+        int rows=grid.size();
+        int cols=grid[0].size();
         int ans=0;
 
+         vector<vector<int>> vis(rows, vector<int>(cols, 0));
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(isBoundary(i,j,n,m) && grid[i][j]==1 && !visited[i][j]){
-                
-                  dfs(i,j,visited,grid);
-                }
-            }
+         for (int j = 0; j < cols; j++) {
+            if (!vis[0][j] && grid[0][j] == 1)
+                dfs(0, j, vis, grid);
+
+            if (!vis[rows - 1][j] && grid[rows - 1][j] == 1)
+                dfs(rows - 1, j, vis, grid);
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1){
+        for (int i = 0; i < rows; i++) {
+            if (!vis[i][0] && grid[i][0] == 1)
+                dfs(i, 0, vis, grid);
+
+            if (!vis[i][cols - 1] && grid[i][cols - 1] == 1)
+                dfs(i, cols - 1, vis, grid);
+        }
+
+    
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                if (grid[i][j] == 1 && vis[i][j] == 0) {
                     ans++;
                 }
             }
         }
-        
 
         return ans;
+
     }
 };
