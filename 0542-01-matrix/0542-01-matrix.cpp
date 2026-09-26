e0a88de85a8e1f4d6d976f8afe7bec49a1f365bf
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2026-09-26 23:37:47 +0530

    Time: 18 ms (57.63%), Space: 34.7 MB (84.41%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,39 +1,51 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-         int n=mat.size();
-         int m=mat[0].size();
+        int n=mat.size();
+        int m=mat[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, -1));
 
-         vector<vector<int>> dist(n,vector<int>(m,-1));
-         queue<pair<int,int>>q;
 
-         for(int i=0;i<n;i++){
+
+        queue<pair<int,int>> q;
+
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q.push({i,j});
                     dist[i][j]=0;
-                }
+                                    }
             }
-         }
+        }
 
-         int dcol[]={-1,0,+1,0};
-         int drow[]={0,+1,0,-1};
+        int dr[]={-1,0,+1,0};
+        int dc[]={0,+1,0,-1};
 
-         while(!q.empty()){
-            int row=q.front().first;
-            int col=q.front().second;
+        while(!q.empty()){
+            auto [r,c]=q.front();
             q.pop();
-            for(int i=0;i<4;i++){
-                  int nr = row + drow[i];
-                 int nc = col + dcol[i];
 
-                 if(nr>=0 && nr<n && nc>=0 && nc<m  && dist[nr][nc] == -1){
-                    q.push({nr,nc});
-                    dist[nr][nc]=dist[row][col]+1;    
-                 }
+            for(int i=0;i<4;i++){
+                int nr=r+dr[i];
+                int nc=c+dc[i];
+
+                 if (nr < 0 || nc < 0 || nr >= n || nc >= m) {
+                    continue;
+                }
+
+            
+                if (dist[nr][nc] != -1) {
+                    continue;
+                }
+
+            
+                dist[nr][nc] = dist[r][c] + 1;
+
+                q.push({nr, nc});
             }
-         }
+        }
 
         return dist;
+
     }
 };
